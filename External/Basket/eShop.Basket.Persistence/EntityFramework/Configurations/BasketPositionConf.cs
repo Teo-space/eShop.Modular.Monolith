c9c697daad89596bf8123213f8c30748e02e6859
b.a.n <barmin.andrey.nnov@gmail.com>
2024-08-18 13:05:54 +0300

    BasketPosition: Add conf, remove ulid id

---
+++ External/Basket/eShop.Basket.Persistence/EntityFramework/Configurations/BasketPositionConf.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using eShop.Basket.Domain;
+using NUlid;
+
+namespace eShop.Basket.Persistence.EntityFramework.Configurations;
+
+
+internal class BasketPositionConf : IEntityTypeConfiguration<BasketPosition>
+{
+    public void Configure(EntityTypeBuilder<BasketPosition> builder)
+    {
+        builder.HasKey(x => new
+        {
+            x.ClientId,
+            x.ProductId,
+        });
+
+        builder.Property(x => x.ProductName).HasMaxLength(64);
+    }
+}
